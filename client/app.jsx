422c556086892f4f32422cd8f6d4f45c5a11fdc3
Youssef Najjarine <youssefnajjarine@hotmail.com>
2022-02-07 16:20:01 -0800

    Completed user-can-view-students-and-their-info.

---
+++ client/app.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import parseRoute from './lib/parse-route';
+import Header from './pages/header';
+import Students from './pages/students';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      route: parseRoute(window.location.hash)
+    };
+  }
+
+  renderPage() {
+    const { route } = this.state;
+    if (route.path === '' || route.path === 'students') {
+      return <Students/>;
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <div className='container'>
+          <Header/>
+          <main>
+            {this.renderPage()}
+          </main>
+        </div>
+      </>
+    );
+  }
+}
+
+// http://localhost:3000/
